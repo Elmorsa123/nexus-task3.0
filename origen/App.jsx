bacdad8500a4953f0e06518968e568dfbde3d672
Elmorsa123 <99569176+Elmorsa123@users.noreply.github.com>
2025-06-23 20:10:28 +0200

    Create App.jsx

---
+++ origen/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Nexus Task</h1>
+      <p>Bienvenido a tu plataforma de gestión de tareas inteligente!</p>
+    </div>
+  );
+}
+
+export default App;
